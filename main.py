c8dbc86688fac161dbf8b61d59aab4887be11a5f
patriksmt <schmitt.patrik@gmail.com>
2019-01-01 13:21:03 +0100

    Updated

--- main.py
+++ main.py
@@ -4,52 +4,81 @@ import math
 
 class Cell:
 	def __init__(self, row, col):
-		self.remaining_numbers = [1,2,3,4,5,6,7,8,9]
-		self.possibility_space = [1,2,3,4,5,6,7,8,9]
+		self.candidate_numbers = [1,2,3,4,5,6,7,8,9]
 		self.fixed = False
 		self.found = False
 		self.value = '-'
 		self.row = row
 		self.col = col
 
-	def get_possibilities(self):
-		return len(self.remaining_numbers)
-
 	def get_address(self):
 		return [self.row, self.col]
 
-	def remove_possibility(self, possibility):
-		if self.fixed == True or self.found == True:
+	def get_cluster(self):
+		if self.row < 3:
+			if self.col < 3:
+				return 1
+			if self.col >= 3 and self.col <= 5:
+				return 2
+			if self.col > 5:
+				return 3
+
+		if self.row <= 3 and self.row <= 5:
+			if self.col < 3:
+				return 4
+			if self.col >= 3 and self.col <= 5:
+				return 5
+			if self.col > 5:
+				return 6
+
+		if self.row > 5:
+			if self.col < 3:
+				return 7
+			if self.col >= 3 and self.col <= 5:
+				return 8
+			if self.col > 5:
+				return 9
+
+	def remove_candidate(self, possibility):
+		if self.found == True:
 			return
 
+
 		try:
-			self.remaining_numbers.pop(self.remaining_numbers.index(int(possibility)))
-			self.possibility_space[self.possibility_space.index(int(possibility))] = '-'
-			
+			self.candidate_numbers.pop(self.candidate_numbers.index(int(possibility)))	
 		except ValueError:
 			pass
 
-		if self.get_possibilities() == 1:
-			#print(fore.GREEN + 'Only one possibility left. Inserting: {}'.format(self.remaining_numbers[0]) + style.RESET)
-			self.value = self.remaining_numbers[0]
+		if len(self.candidate_numbers) == 1:
+			self.value = self.candidate_numbers[0]
 			self.found = True
-			#trigger new removals
+			print('Found a new number')
 
 	def set_fixed(self, number):
 		self.value = number
 		self.fixed = True
 		self.found = True
-		for n in range(0,9):
-			if n + 1 != int(number):
-				self.possibility_space[n] = '-'
+		self.candidate_numbers = [int(number)]
 
 	def set_number(self, number):
 		self.value = number
-		self.remaining_numbers = [int(number)]
+		self.candidate_numbers = [int(number)]
 		self.found = True
-		for n in range(0,9):
-			if n + 1 != int(number):
-				self.possibility_space[n] = '-'
+		print('Found a new number')
+
+	def get_candidates(self):
+		return self.candidate_numbers
+
+	def get_printable_candidates(self):
+		printable_candidates = [1,2,3,4,5,6,7,8,9]
+		for i in range(0,9):
+			if (i + 1) not in self.candidate_numbers:
+				printable_candidates[i] = '-'
+
+		return printable_candidates
+
+
+
 
 		
 
@@ -59,38 +88,60 @@ class Board:
 	def __init__(self):
 		self.board = []   
 		for i in range (0, 9):  
-		    new = []        
+		    row = []        
 		    for j in range (0, 9): 
-		        new.append(Cell(i, j))  
-		    self.board.append(new)
+		        row.append(Cell(i, j))  
+		    self.board.append(row)
 
-	def eliminate_row(self, number, row):
+	def check_if_single(self, row, col):
+		if len(self.board[row][col].get_candidates) == 1:
+			return True
+			
+
+	def remove_row(self, number, row, ignore_list):
 		for i in range(0,9):
-			#print('(Row) Removing {} from row {}. Currently in col {}'.format(number, row, i))
-			self.board[row][i].remove_possibility(number)
+			if i in ignore_list:
+				continue
+			else:	
+				self.board[row][i].remove_candidate(number)
 
 
-	def eliminate_col(self, number, col):
+	def remove_col(self, number, col, ignore_list):
 		for i in range(0,9):
-			#print('(Col) Removing {} from col {}. Currently in row {}'.format(number, col, i))
-			self.board[i][col].remove_possibility(number)
+			if i in ignore_list:
+				continue
+			else:
+				self.board[i][col].remove_candidate(number)
 
-	def eliminate_cluster(self, number, row, col):
+	def remove_cluster(self, number, row, col, ignore_list):
 		row_cluster = math.floor(row / 3) * 3
 		col_cluster = math.floor(col / 3) * 3
 
 		for i in range(row_cluster, row_cluster + 3):
 			for j in range(col_cluster, col_cluster + 3):
-				#print('(Cluster) Removing {} from row {} and col {}'.format(number, i, j))
-				self.board[i][j].remove_possibility(number)
+				if [i,j] in ignore_list:
+					continue
+				else:
+					self.board[i][j].remove_candidate(number)
 
+	def basic_eliminations(self, number, row, col):
+		for i in range(0,9):
+			self.board[row][i].remove_candidate(number)
+			self.board[i][col].remove_candidate(number)
+
+		row_cluster = math.floor(row / 3) * 3
+		col_cluster = math.floor(col / 3) * 3
+
+		for i in range(row_cluster, row_cluster + 3):
+			for j in range(col_cluster, col_cluster + 3):
+				self.board[i][j].remove_candidate(number)
 
 	def evaluate_cluster(self, cluster_row, cluster_col):
 		overlap_set = [0,0,0,0,0,0,0,0,0]
 		for i in range(cluster_row, cluster_row + 3):
 			for j in range(cluster_col, cluster_col + 3):
 				if self.board[i][j].value == '-':
-					for n in self.board[i][j].remaining_numbers:
+					for n in self.board[i][j].candidate_numbers:
 						overlap_set[n - 1] += 1
 
 		magic_number = 0
@@ -104,11 +155,11 @@ class Board:
 				for i in range(cluster_row, cluster_row + 3):
 					for j in range(cluster_col, cluster_col + 3):
 						if self.board[i][j].value == '-':
-							for n in self.board[i][j].remaining_numbers:
+							for n in self.board[i][j].candidate_numbers:
 								if n == magic_number + 1:
 									print(magic_number)
 									print('hello')
-									print(self.board[i][j].remaining_numbers)
+									print(self.board[i][j].candidate_numbers)
 									print('In row {}, col {}, we found a unique {}'.format(i,j,magic_number+1))
 									self.board[i][j].set_number(magic_number+1)
 						
@@ -119,7 +170,64 @@ class Board:
 				print('evaluating cluster starting in {}, {}'.format(i*3,j*3))
 				self.evaluate_cluster(i * 3, j * 3)
 
-	
+	def naked_subset(self):
+		for i in range(0,81):
+			for j in range(i + 1, 81):
+
+				base_row = math.floor(i / 9)
+				base_col = i - (math.floor(i / 9)) * 9
+
+				compare_row = math.floor(j / 9)
+				compare_col = j - (math.floor(j / 9)) * 9
+
+				if self.board[base_row][base_col].get_candidates() == self.board[compare_row][compare_col].get_candidates() and len(self.board[compare_row][compare_col].get_candidates()) == 2:
+					# print('Found a match')
+					# print(self.board[base_row][base_col].get_candidates())
+					# print('Between: {} and {}, and {} and {}'.format(base_row, base_col, compare_row, compare_col))
+					
+					#Check if the two cells are in the same row, col, or box. If so, remove all other candidates
+					if base_row == compare_row:
+						for n in self.board[base_row][base_col].get_candidates():
+							ignore_list = [base_col, compare_col]
+							self.remove_row(n, base_row, ignore_list)
+
+					if base_col == compare_col:
+						for n in self.board[base_row][base_col].get_candidates():
+							ignore_list = [base_row, compare_row]
+							self.remove_col(n, base_col, ignore_list)
+
+					if self.board[base_row][base_col].get_cluster() == self.board[compare_row][compare_col].get_cluster():
+						for n in self.board[base_row][base_col].get_candidates():
+							ignore_list = [[base_row, base_col], [compare_row, compare_col]]
+							self.remove_cluster(n, base_row, base_col, ignore_list)
+
+
+	def sum_sets(self):
+		tree = {}
+		
+		for i in range(0,81):
+			row = math.floor(i / 9)
+			col = i - (math.floor(i / 9)) * 9
+
+			if len(self.board[row][col].get_candidates()) == 1:
+				continue
+
+			item_list = ''
+			for n in self.board[row][col].get_candidates():
+				item_list = item_list + str(n)
+
+			if item_list not in tree:
+				tree[item_list] = 1
+			else:	
+				tree[item_list] += 1
+
+		print(tree)
+
+
+
+
+
+
 	def render_board(self):		
 		for i in range(0,37):
 			print(fore.RED + "-" + style.RESET, end='')
@@ -145,8 +253,7 @@ class Board:
 			print()
 
 	def render_possibility_space(self, row, col):
-		a = self.board[int(row)][int(col)].possibility_space
-		print(fore.BLUE + 'Possibility space for row {} col {}'.format(row, col) + style.RESET)
+		a = self.board[int(row)][int(col)].get_printable_candidates()
 		print('{} {} {}'.format(a[0],a[1],a[2]))
 		print('{} {} {}'.format(a[3],a[4],a[5]))
 		print('{} {} {}'.format(a[6],a[7],a[8]))
@@ -160,14 +267,22 @@ class Board:
 			for j in range(0,3):
 				print(fore.RED + '| ' + style.RESET, end='')	
 				for i in range(0,9):
-					print(self.board[k][i].possibility_space[j*3], end='')
+					if self.board[k][i].found == True:
+						print(fore.GREEN, end='')
+					if self.board[k][i].fixed == True:
+						print(fore.BLUE, end='')
+					print(self.board[k][i].get_printable_candidates()[j*3], end='')
 					print(' ', end='')
-					print(self.board[k][i].possibility_space[j*3+1], end='')
+					print(self.board[k][i].get_printable_candidates()[j*3+1], end='')
 					print(' ', end='')
-					print(self.board[k][i].possibility_space[j*3+2], end='')
-					print(fore.RED + ' | ' + style.RESET, end='')
+					print(self.board[k][i].get_printable_candidates()[j*3+2], end='')
+					print(style.RESET, end='')
+					if (i + 1) % 3 == 0:
+						print(fore.RED + ' | ' + style.RESET, end='')
+					else:
+						print(' | ', end='')
 				print()
-			if (k-2) % 3 == 0:
+			if (k + 1) % 3 == 0:
 				for i in range(0,73):
 					print(fore.RED + "-" + style.RESET, end='')
 			else:
@@ -184,8 +299,6 @@ class Board:
 		col_num = 0
 
 		for l in f:
-			
-			
 			col_num = 0
 			for n in l:
 				if n != '-' and n != '\n':
@@ -198,20 +311,20 @@ class Board:
 
 	def solve(self):
 		rotation = 0
-		while rotation != 10:
-			print(rotation)
-			rotation += 1
-			for i in range(0,9):
-				for j in range(0,9):
-					if self.board[i][j].value != '-':
-						#print('Value {} in Row {} Col {}'.format(self.board[i][j].value, i,j))
-						self.eliminate_row(self.board[i][j].value, i)
-						self.eliminate_col(self.board[i][j].value, j)
-						self.eliminate_cluster(self.board[i][j].value, i, j)
-						#Need to get input if a new number was found. If so we need to rerun the eliminations
+		#while rotation != 10:
+		rotation += 1
+		for i in range(0,9):
+			for j in range(0,9):
+				if self.board[i][j].value != '-':
+					self.basic_eliminations(self.board[i][j].value, i, j)
+					#Need to get input if a new number was found. If so we need to rerun the eliminations
 
 
-
+	def full_solve(self):
+		pass
+		#1. basic eliminations
+		#2. Unique evaluations
+		#3. Subsets
 
 			
 
@@ -224,32 +337,32 @@ while True:
 	raw_inst = input('Instruction: ')
 	inst = raw_inst[:4]
 
-	if inst == 'rend':
+	#INPUT FUNCTIONS
+	if inst == 'l': #l - Load
+		b.read_file()
+
+	#SOLVE FUNCTIONS
+	if inst == 's': #s - Solve
+		b.solve()
+	if inst == 'e': #e - Evaluate
+		b.evaluate_clusters()
+
+	#RENDER FUNCTIONS
+	if inst == 'r': #r - Render
 		b.render_board()
-	if inst == 'sspa':
+	if inst == 'c': #c - render Candidates
+		b.render_solution_space()
+	if inst == 'li': #l - render local candidates
 		row = input('Row: ')
 		col = input('Column: ')
 		b.render_possibility_space(row, col)
-	if inst == 'read':
-		b.read_file()
+	if inst == 'ss':
+		b.sum_sets()
+	
+	#MISC FUNCTIONS
 	if inst == 'exit' or inst == 'quit':
 		break
-	if inst == 'solv':
-		b.solve()
 
-	if inst == 'eval':
-		b.evaluate_clusters()
-	if inst == 'full':
-		b.read_file()
-		b.solve()
-		b.evaluate_clusters()
-	if inst == 'rspa':
-		b.render_solution_space()
+	if inst == 'n':
+		b.naked_subset()
 	
-
-
-
-
-
-if __name__ == "__main__":
-	a = SudukoBoard()
